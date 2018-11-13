799c7b6d46041ea07f26b14d82bd9b974e6bc00f
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-11-13 12:27:29 -0300

    Referencing from the interface.

--- src/main/java/translator/application/EntryPoint.java
+++ src/main/java/translator/application/EntryPoint.java
@@ -2,12 +2,13 @@ package translator.application;
 
 import translator.controller.ControllerImpl;
 import translator.model.Translator;
+import translator.view.View;
 import translator.view.swing.ViewSwing;
 
 public class EntryPoint {
     public static void main(String[] args) {
         Translator translator = new Translator();
-        ViewSwing view = new ViewSwing();
+        View view = new ViewSwing();
         ControllerImpl controller = new ControllerImpl(translator, view);
         controller.start();
     }
